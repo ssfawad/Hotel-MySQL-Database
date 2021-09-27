61a601a938f2f1c674a5e5ff1b9625be46e8e301
Syed <syedsfawad@gmail.com>
2021-09-27 08:56:38 -0500

    Update SyedFawad-HotelDB.sql

--- SyedFawad-HotelDB.sql
+++ SyedFawad-HotelDB.sql
@@ -4,40 +4,40 @@ USE Hotel;
 
 CREATE TABLE Guest (
 	GuestID INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(50) NOT NULL,
-    Address VARCHAR(50) NOT NULL,
-    City VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL,
-    Zip VARCHAR(5) NOT NULL,
-    Phone VARCHAR(15) NOT NULL
-    );
+	`Name` VARCHAR(50) NOT NULL,
+    	Address VARCHAR(50) NOT NULL,
+    	City VARCHAR(50) NOT NULL,
+    	State VARCHAR(50) NOT NULL,
+    	Zip VARCHAR(5) NOT NULL,
+    	Phone VARCHAR(15) NOT NULL
+);
 
 CREATE TABLE Room (
 	RoomNum INT PRIMARY KEY,
-    RoomType VARCHAR(10) NOT NULL,
-    ADA BOOLEAN NOT NULL,
-    Amenity1 VARCHAR(25) NOT NULL,
+    	RoomType VARCHAR(10) NOT NULL,
+    	ADA BOOLEAN NOT NULL,
+    	Amenity1 VARCHAR(25) NOT NULL,
 	Amenity2 VARCHAR(25) NULL,
-    Amenity3 VARCHAR(25) NULL,
-    StandardOccupant INT NOT NULL,
-    MaxOccupant INT NOT NULL,
-    BasePrice DECIMAL(5,2) NOT NULL,
-    ExraPerson DECIMAL (3,0) NULL
-    );
+	Amenity3 VARCHAR(25) NULL,
+	StandardOccupant INT NOT NULL,
+	MaxOccupant INT NOT NULL,
+	BasePrice DECIMAL(5,2) NOT NULL,
+	ExraPerson DECIMAL (3,0) NULL
+);
         
 CREATE TABLE Reservation (
 	ReservationID INT PRIMARY KEY AUTO_INCREMENT,
-    GuestID INT NOT NULL,
-    RoomNum INT NOT NULL,
+	GuestID INT NOT NULL,
+	RoomNum INT NOT NULL,
 	FOREIGN KEY fk_Reservation_Guest (GuestID)
 		REFERENCES Guest(GuestID),
 	FOREIGN KEY fk_Reservation_Room (RoomNum)
 		REFERENCES Room(RoomNum),
 	Adults INT NOT NULL,
-    Children INT NULL,
-    StartDate DATE NOT NULL,
-    EndDate DATE NOT NULL,
-    TotalCost DECIMAL(6,2)
-	);
+	Children INT NULL,
+	StartDate DATE NOT NULL,
+	EndDate DATE NOT NULL,
+	TotalCost DECIMAL(6,2)
+);
 
     
